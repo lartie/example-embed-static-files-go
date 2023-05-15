09e4155c8898d27a17090fa1b35d1e07523f37e9
lartie <log.wil.log@gmail.com>
2023-05-15 23:24:40 +0300

    add metadata

--- internal/box/box.go
+++ internal/box/box.go
@@ -2,28 +2,48 @@
 
 package box
 
+import (
+	"os"
+	"strings"
+)
+
+type FileInfo struct {
+	Name    string      `json:"name"`
+	Content []byte      `json:"content"`
+	Perm    os.FileMode `json:"perm"`
+}
+
 type embedBox struct {
-	storage map[string][]byte
+	storage map[string]FileInfo
 }
 
 // Create new box for embed files
 func newEmbedBox() *embedBox {
-	return &embedBox{storage: make(map[string][]byte)}
+	return &embedBox{storage: make(map[string]FileInfo)}
 }
 
 // Add a file to box
-func (e *embedBox) Add(file string, content []byte) {
-	e.storage[file] = content
+func (e *embedBox) Add(file string, fileInfo FileInfo) {
+	e.storage[file] = fileInfo
 }
 
 // Get file's content
 // Always use / for looking up
 // For example: /init/README.md is actually configs/init/README.md
-func (e *embedBox) Get(file string) []byte {
-	if f, ok := e.storage[file]; ok {
-		return f
+func (e *embedBox) Get(file string) []FileInfo {
+	var files []FileInfo
+	if strings.Contains(file, "*") {
+		for s, info := range e.storage {
+			if strings.HasPrefix(s, strings.TrimRight(file, "*")) {
+				files = append(files, info)
+			}
+		}
+	} else {
+		if f, ok := e.storage[file]; ok {
+			files = append(files, f)
+		}
 	}
-	return nil
+	return files
 }
 
 // Find for a file
@@ -38,12 +58,12 @@ func (e *embedBox) Has(file string) bool {
 var box = newEmbedBox()
 
 // Add a file content to box
-func Add(file string, content []byte) {
-	box.Add(file, content)
+func Add(file string, fileInfo FileInfo) {
+	box.Add(file, fileInfo)
 }
 
 // Get a file from box
-func Get(file string) []byte {
+func Get(file string) []FileInfo {
 	return box.Get(file)
 }
 
